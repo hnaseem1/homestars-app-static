d568ce046518848455c433a2e3df79bd1daa39b6
Hasan Naseem <hn92@live.com>
2020-10-27 17:28:25 -0400

    setting up Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('App renders without crashing', () => {
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
