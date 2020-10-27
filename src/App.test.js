a8a9330ca3b3b080a40c024fe082d53c0d7b462f
Hasan Naseem <hn92@live.com>
2020-10-27 17:37:48 -0400

    enzyme adapter added tests passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import { shallow } from 'enzyme';
+
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+
+configure({ adapter: new Adapter() });
 
 it('App renders without crashing', () => {
   const component = shallow(<App />);
