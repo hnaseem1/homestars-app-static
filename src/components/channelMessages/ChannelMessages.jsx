6b380d8bf12413d58d73604a0ced8016f2dbd314
Hasan Naseem <hn92@live.com>
2020-10-28 10:28:15 -0400

    added unique key for each message

--- src/components/channelMessages/ChannelMessages.jsx
+++ src/components/channelMessages/ChannelMessages.jsx
@@ -14,7 +14,7 @@ const ChannelMessage = ({userId, channelId}) => {
             const response = await fetch(`${APIURL}/channels/${channelId}/channel_messages`);
             const body = await response.json();
             if (!unmounted) {
-                setMessages(body.map(({ message, created_at }) => ({ message: message, created_at: created_at })));
+                setMessages(body.map(({ message, created_at, id }) => ({ message: message, created_at: created_at, id: id })));
                 setLoading(false);
             }
         }
@@ -28,8 +28,8 @@ const ChannelMessage = ({userId, channelId}) => {
     return (
         <div name="messages" id="messages" disabled={loading} >
         {
-            messages.map(({ message, created_at }) => (
-                <div key={message}> 
+            messages.map(({ message, created_at, id }) => (
+                <div key={id}> 
                     <p>{message}</p>
                     <span class="time-right">{created_at}</span>
                 </div>
